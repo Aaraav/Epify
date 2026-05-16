6c0ab1c42b3162e3d877c2dd5058bdc720d2fa8f
Aaraav <aaraav2810@gmail.com>
2026-05-16 13:08:18 +0530

    removed timout

--- Connections.js
+++ Connections.js
@@ -2,21 +2,19 @@ import mongoose from 'mongoose';
 import { createClient } from 'redis';
 import dotenv from 'dotenv';
 dotenv.config();
-// 1. Initialize Redis Client instance
 export const redisClient = createClient({
     url: process.env.REDIS_URL,
     socket: {
-        tls: process.env.REDIS_URL?.startsWith('rediss://'), // auto-detect
+        tls: process.env.REDIS_URL?.startsWith('rediss://'), 
         rejectUnauthorized: false
     }
 
 });
 
-console.log('REDIS_URL:', process.env.REDIS_URL); // remove after fixing
 
-redisClient.on('error', (err) => console.error('❌ Redis Client Error:', err));
+redisClient.on('error', (err) => console.error(' Redis Client Error:', err));
 
-// 2. Main Infrastructure Bootstrapper
+// Main Infrastructure Bootstrapper
 const connectInfrastructure = async () => {
     try {
         // Connect to MongoDB
